5916f66802cda4712e121983a629ed9466f42368
JenGates3255 <Jengates3255@gmail.com>
2014-06-27 08:51:18 -0600

    1and2 complete

---
+++ main.js
@@ -0,0 +1,41 @@
+$(document).on('ready', function() {
+  
+});
+
+$(".b1").click (function() {
+	console.log("Herrow");
+	$("body").append("Hey There!");
+});
+
+$(".b2").click (function(){
+ $("body").append("<h1> YYYeaaaaaahhh </h1>");
+});
+
+$(".b3").click (function(){
+ $("body").append("<ul><li>zoey</li><li>is</li><li>amazing</li></ul>");
+});
+
+// $("p").mouseover(function(){
+//  $(this).css("color","pink");
+// }).mouseout(function(){
+//  $(this).css("color","black");
+// });
+
+$("p").hover(function(){
+ $(this).css("color","pink");
+}, function(){
+ $(this).css("color","black");
+});
+
+$(document).on ('ready',function(){
+ $(".heading").append("<em>!</em>")
+});
+
+
+$(".link").click(function(){
+ var meow = confirm("Are you sure?");
+	if (meow === false){
+ 	$(this).remove();
+	} 
+ return meow;
+});
