56220e2064260526fccae97c8f786380724be2d0
JenGates3255 <Jengates3255@gmail.com>
2014-06-27 12:30:55 -0600

    Complete

--- main.js
+++ main.js
@@ -1,41 +1,52 @@
-$(document).on('ready', function() {
+// $(document).on('ready', function() {
   
-});
+// });
 
-$(".b1").click (function() {
-	console.log("Herrow");
-	$("body").append("Hey There!");
-});
+// $(".b1").click (function() {
+// 	console.log("Herrow");
+// 	$("body").append("Hey There!");
+// });
 
-$(".b2").click (function(){
- $("body").append("<h1> YYYeaaaaaahhh </h1>");
-});
+// $(".b2").click (function(){
+//  $("body").append("<h1> YYYeaaaaaahhh </h1>");
+// });
 
-$(".b3").click (function(){
- $("body").append("<ul><li>zoey</li><li>is</li><li>amazing</li></ul>");
-});
+// $(".b3").click (function(){
+//  $("body").append("<ul><li>zoey</li><li>is</li><li>amazing</li></ul>");
+// });
 
-// $("p").mouseover(function(){
+// // $("p").mouseover(function(){
+// //  $(this).css("color","pink");
+// // }).mouseout(function(){
+// //  $(this).css("color","black");
+// // });
+
+// $("p").hover(function(){
 //  $(this).css("color","pink");
-// }).mouseout(function(){
+// }, function(){
 //  $(this).css("color","black");
 // });
 
-$("p").hover(function(){
- $(this).css("color","pink");
-}, function(){
- $(this).css("color","black");
-});
-
-$(document).on ('ready',function(){
- $(".heading").append("<em>!</em>")
-});
+// $(document).on ('ready',function(){
+//  $(".heading").append("<em>!</em>")
+// });
 
 
-$(".link").click(function(){
- var meow = confirm("Are you sure?");
-	if (meow === false){
- 	$(this).remove();
-	} 
- return meow;
+// $(".link").click(function(){
+//  var meow = confirm("Are you sure?");
+// 	if (meow === false){
+//  	$(this).remove();
+// 	} 
+//  return meow;
+// });
+//..............................start big button
+
+$('.button').click(function(){
+  $('body').append('<div class="backdrop"><div class="box"> Light box<div class="close"><button>X</button></div></div></div>');
+  	$('.close').click(function(){
+  		$('.backdrop').remove();
+  	});
 });
+
+
+
